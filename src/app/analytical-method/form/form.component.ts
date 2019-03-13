b8d9f475bfa421cdbc3d0108b2d98355989238f9
Soumyarup Banik <soumyarupb@MPTDC1-India.MirrorPlus.com>
2019-03-13 18:14:04 +0530

    MOC addition and CSS styling

--- src/app/analytical-method/form/form.component.ts
+++ src/app/analytical-method/form/form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AnalyticalService } from '../analytical.service';
 
+declare var $: any;
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -13,6 +15,12 @@ export class FormComponent implements OnInit {
     'Bioburden',
     'Endotoxin'
   ];
+  MOCList: any[] = [
+    'Stainless Steel',
+    'Glass',
+    'Teflon',
+    'Plastic'
+  ];
   isSwabCollapsed: boolean;
   isRinseCollapsed: boolean;
   rinseConfigure: String;
@@ -24,106 +32,117 @@ export class FormComponent implements OnInit {
   curMethodData: any;
   isCreate: boolean;
   isMocAdded: boolean;
+  isRinseMocAdded: boolean;
   mocCount: number;
+  swabMoc: String;
+  height: number;
 
-  constructor(private analyticalService: AnalyticalService) { }
+  constructor(private analyticalService: AnalyticalService) {
+    setTimeout(() => this.heightCalculation(), 200);
+   }
 
   ngOnInit() {
     this.resetForm();
     this.resetData();
     this.analyticalService.edit.subscribe(method => {
-      this.isCreate = false
+      this.isCreate = false;
       this.curMethodData = this.getAnalyticalMethod(method);
       this.selectedSubstance = this.curMethodData.residueType;
       console.log(this.selectedSubstance);
-    })
+    });
     this.analyticalService.create.subscribe(() => {
       this.isCreate = true;
       this.resetData();
-    })
+    });
+  }
+
+  heightCalculation() {
+    const cheight = $('.container').height();
+    this.height = (cheight - 149);
   }
 
   resetForm() {
     this.isCreate = true;
     this.isSwabCollapsed = true;
-    this.rinseConfigure = 'Configure'
+    this.rinseConfigure = 'Configure';
     this.isRinseCollapsed = true;
     this.swabConfigure = 'Configure';
     this.tLimitChecked = true;
     this.selectedSubstance = this.Substances[0];
-    this.rinseLimitChecked = true
+    this.rinseLimitChecked = true;
     this.swabLimitChecked = true;
     this.isMocAdded = false;
+    this.isRinseMocAdded = false;
     this.mocCount = 0;
+    this.swabMoc = this.MOCList[0];
   }
 
   resetData() {
     this.curMethodData = {
-      "analyticalMethodId": undefined,
-      "residueType": undefined,
-      "LOD": undefined,
-      "LOQ": undefined,
-      "reason": undefined,
-      "usedMethod": undefined,
-      "tLimit": true,
-      "TNTC": undefined,
-      "TFTC": undefined,
-      "SwabSample": {
-        "usedMethod": undefined,
-        "solventName": undefined,
-        "solventQuantity": undefined,
-        "defaultRecovery": undefined,
-        "isSwabRecovery": true,
-        "swabRecovery": undefined,
-        "moc": [
+      'analyticalMethodId': undefined,
+      'residueType': this.Substances[0],
+      'LOD': undefined,
+      'LOQ': undefined,
+      'reason': undefined,
+      'usedMethod': undefined,
+      'tLimit': true,
+      'TNTC': undefined,
+      'TFTC': undefined,
+      'SwabSample': {
+        'usedMethod': undefined,
+        'solventName': undefined,
+        'solventQuantity': undefined,
+        'defaultRecovery': undefined,
+        'isSwabRecovery': true,
+        'swabRecovery': undefined,
+        'moc': [
         ]
       },
-      "RinseSample": {
-        "usedMethod": undefined,
-        "defaultRecovery": undefined,
-        "rinseSolventVolume": undefined,
-        "isRinseRecovery":true,
-        "rinseRecovery": undefined,
-        "moc": [
+      'RinseSample': {
+        'usedMethod': undefined,
+        'defaultRecovery': undefined,
+        'rinseSolventVolume': undefined,
+        'isRinseRecovery': true,
+        'rinseRecovery': undefined,
+        'moc': [
         ]
       }
     };
   }
 
   toggleRinse() {
-    this.isRinseCollapsed = !this.isRinseCollapsed
+    this.isRinseCollapsed = !this.isRinseCollapsed;
+    this.isRinseMocAdded = false;
     if (this.isRinseCollapsed) {
       this.rinseConfigure = 'Configure';
       this.curMethodData.RinseSample = {
-        "usedMethod": undefined,
-        "defaultRecovery": undefined,
-        "rinseSolventVolume": undefined,
-        "isRinseRecovery":true,
-        "rinseRecovery": undefined,
-        "moc": [
+        'usedMethod': undefined,
+        'defaultRecovery': undefined,
+        'rinseSolventVolume': undefined,
+        'isRinseRecovery': true,
+        'rinseRecovery': undefined,
+        'moc': [
         ]
-      }
-    }
-    else this.rinseConfigure = 'Remove';
+      };
+    } else { this.rinseConfigure = 'Remove'; }
   }
 
   toggleSwab() {
     this.isSwabCollapsed = !this.isSwabCollapsed;
     this.isMocAdded = false;
-    if (this.isSwabCollapsed){
+    if (this.isSwabCollapsed) {
       this.swabConfigure = 'Configure';
       this.curMethodData.SwabSample = {
-        "usedMethod": undefined,
-        "solventName": undefined,
-        "solventQuantity": undefined,
-        "defaultRecovery": undefined,
-        "isSwabRecovery": true,
-        "swabRecovery": undefined,
-        "moc": [
+        'usedMethod': undefined,
+        'solventName': undefined,
+        'solventQuantity': undefined,
+        'defaultRecovery': undefined,
+        'isSwabRecovery': true,
+        'swabRecovery': undefined,
+        'moc': [
         ]
-      }
-    }
-    else this.swabConfigure = 'Remove'
+      };
+    } else { this.swabConfigure = 'Remove'; }
   }
 
   getAnalyticalMethod(method) {
@@ -142,24 +161,49 @@ export class FormComponent implements OnInit {
     this.resetData();
   }
 
-  selectResidue(substance){
-    this.selectedSubstance = substance; 
+  selectResidue(substance) {
+    this.selectedSubstance = substance;
     this.curMethodData.residueType = substance;
   }
 
-  addMOC(){
+  addMOC() {
     this.isMocAdded = true;
+    // this.swabMoc = this.MOCList[0];
     this.curMethodData.SwabSample.moc.push({
-      "mocType": undefined,
-      "recovery": undefined
-    })
+      'mocType': this.MOCList[0],
+      'recovery': undefined
+    });
   }
 
-  removeMOC(moc){
-    if(this.curMethodData.SwabSample.moc.includes(moc)){
-      this.curMethodData.SwabSample.moc.splice(moc,1);
+  addRinseMOC() {
+    this.isRinseMocAdded = true;
+    // this.swabMoc = this.MOCList[0];
+    this.curMethodData.RinseSample.moc.push({
+      'mocType': this.MOCList[0],
+      'recovery': undefined
+    });
+  }
+
+  removeMOC(moc, idx) {
+    if (this.curMethodData.SwabSample.moc.includes(moc)) {
+      this.curMethodData.SwabSample.moc.splice(idx, 1);
     }
-    if(!this.curMethodData.SwabSample.moc.length) this.isMocAdded = false;
+    if (!this.curMethodData.SwabSample.moc.length) { this.isMocAdded = false; }
+  }
+
+  removeRinseMOC(moc, idx) {
+    if (this.curMethodData.RinseSample.moc.includes(moc)) {
+      this.curMethodData.RinseSample.moc.splice(idx, 1);
+    }
+    if (!this.curMethodData.RinseSample.moc.length) { this.isRinseMocAdded = false; }
+  }
+
+  selectMocType(moc, idx) {
+    this.curMethodData.SwabSample.moc[idx].mocType = moc;
+  }
+
+  selectRinseMocType(moc, idx) {
+    this.curMethodData.RinseSample.moc[idx].mocType = moc;
   }
 
 }
--- src/app/analytical-method/layout/layout.component.ts
+++ src/app/analytical-method/layout/layout.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('header') header: ElementRef;
+  @ViewChild('header') footer: ElementRef;
+  height: any;
+  constructor() {
+    setTimeout(() => this.heightCalculation(), 200);
+  }
 
   ngOnInit() {
+  }
+  heightCalculation() {
+    const winHeight = $(window).height();
+    this.height = (winHeight - 120);
   }
 
 }
